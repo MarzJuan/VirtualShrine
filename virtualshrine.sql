d607933228255a4e37741a90769a4a10a8e421a2
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-09-17 10:34:23 +0800

    Update virtualshrine.sql

--- virtualshrine.sql
+++ virtualshrine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 14, 2022 at 11:38 AM
+-- Generation Time: Sep 17, 2022 at 04:33 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -37,15 +37,17 @@ CREATE TABLE `bookings` (
   `org_name` varchar(200) NOT NULL,
   `date_visit` varchar(100) NOT NULL,
   `time_visit` varchar(100) NOT NULL,
-  `no_visitors` int(50) NOT NULL
+  `no_visitors` int(50) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 = Pending\r\n1 = Approved\r\n2 = Rejected'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `bookings`
 --
 
-INSERT INTO `bookings` (`booking_id`, `fname`, `lname`, `phone_no`, `email`, `id_image`, `org_name`, `date_visit`, `time_visit`, `no_visitors`) VALUES
-(1, 'Visitor', 'Sample', '09970639196', 'kjnacional11@gmail.com', '', 'Bulsu', 'September 20, 2022', '10:00', 4);
+INSERT INTO `bookings` (`booking_id`, `fname`, `lname`, `phone_no`, `email`, `id_image`, `org_name`, `date_visit`, `time_visit`, `no_visitors`, `status`) VALUES
+(1, 'Visitor', 'Sample', '09970639196', 'kjnacional11@gmail.com', '', 'Bulsu', 'September 20, 2022', '10:00', 4, 1),
+(2, 'Samantha', 'Raine', '09021989876', 'kjnacionalqqsa@gmail.com', '', 'Antique National School', '2019115479', '8:50', 4, 2);
 
 -- --------------------------------------------------------
 
@@ -61,10 +63,19 @@ CREATE TABLE `categories` (
   `meta_title` varchar(200) NOT NULL,
   `meta_description` mediumtext DEFAULT NULL,
   `meta_keyword` mediumtext DEFAULT NULL,
-  `navbar_status` tinyint(1) DEFAULT 0,
-  `status` tinyint(1) DEFAULT 0,
+  `navbar_status` tinyint(1) DEFAULT 0 COMMENT '0 = visible\r\n1 = hidden',
+  `status` tinyint(1) DEFAULT 0 COMMENT '0 = Visible\r\n1 = Hidden\r\n2 = Archived',
   `created_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `slug`, `description`, `meta_title`, `meta_description`, `meta_keyword`, `navbar_status`, `status`, `created_at`) VALUES
+(1, 'Homepage', 'homepage', 'VirtualShrine Homepage', 'Homepage', 'VirtualShrine Homepage', 'VirtualShrine Homepage', 1, 0, '2022-09-14 16:38:13'),
+(2, 'Sample Category', 'Sample-category', 'This is a sample category', 'Sample Category', 'This is a sample category', 'sample category', 1, 0, '2022-09-14 23:03:23'),
+(3, 'Visit', 'virtual-shrine-visit', 'Visit category', 'Visit', 'Visit category', 'Visit', 0, 0, '2022-09-17 02:18:06');
 
 -- --------------------------------------------------------
 
@@ -82,9 +93,17 @@ CREATE TABLE `posts` (
   `meta_title` varchar(200) NOT NULL,
   `meta_description` mediumtext NOT NULL,
   `meta_keyword` mediumtext NOT NULL,
-  `status` tinyint(1) NOT NULL,
+  `status` tinyint(1) DEFAULT 0 COMMENT '0 = Active\r\n1 = Archive',
   `created_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `category_id`, `name`, `slug`, `description`, `image`, `meta_title`, `meta_description`, `meta_keyword`, `status`, `created_at`) VALUES
+(4, 1, 'Homepage Updated', 'homepage', 'sample homepage', '1663366180.png', 'Homepage', 'sample homepage', 'sample homepage', 0, '2022-09-14 23:19:27'),
+(5, 2, 'Gallery', 'gallery-one', '<p>This is the page post - UPDATE</p>', '', 'Gallery 1', 'This is the gallery 1', 'gallery', 1, '2022-09-16 21:56:22');
 
 -- --------------------------------------------------------
 
@@ -110,7 +129,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `fname`, `lname`, `username`, `password`, `role_as`, `status`, `created_at`) VALUES
 (1, 'Kristine Joy', 'Nacional', 'kristhayne', 'pass1', 0, 0, '2022-09-13 11:34:58'),
 (2, 'Angelo', 'Mauricio', 'gelo', 'pass2', 1, 1, '2022-09-13 11:53:13'),
-(3, 'sample', 'Admin', 'samplead', 'passsample', 1, 1, '2022-09-13 12:24:18');
+(3, 'sample', 'Admin', 'samplead', 'passsample', 1, 1, '2022-09-13 12:24:18'),
+(4, 'Taku', 'Asami', 'takuto', 'password', 1, 0, '2022-09-14 16:36:29');
 
 -- --------------------------------------------------------
 
@@ -165,25 +185,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
