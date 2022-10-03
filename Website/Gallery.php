64d7d49bd4cf0e492d25b965160e1ed426e5df38
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-10-03 21:19:58 +0800

    kristine

--- Website/Gallery.php
+++ Website/Gallery.php
@@ -4,6 +4,7 @@
         <title>Gallery Page</title>
 <link rel="stylesheet" href="style.css">
 <link href="https://fonts.googleapis.com/css2?family=Lora:ital@1&display=swap" rel="stylesheet">
+<link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
 <header class="Gallery-header">
@@ -34,10 +35,10 @@
 <div class="gen-text">Visiting Galleries</div>
 <hr class="plan-line">
 
-<svg class="inline-icon" style="width:53px;height:53px" viewBox="0 0 24 24">
+<!--<svg class="inline-icon" style="width:53px;height:53px" viewBox="0 0 24 24">
         <path fill="currentColor" d="M12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22C6.47,22 2,17.5 2,12A10,10 0 0,1 12,2M12.5,7V12.25L17,14.92L16.25,16.15L11,13V7H12.5Z" />
-</svg>
-<div class="Opening">
+</svg>-->
+<div class="Opening"><i class='bx bxs-time'></i>
         Openning Time
 </div>
 <div class="right-subtext">Tuesday - Sunday (8:00AM - 4:00PM)</div>
