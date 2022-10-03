73569cd0ea571b175ab4efbbb649de7bef0c36a4
Gelomauricio51 <angelo.mauricio.d@bulsu.edu.ph>
2022-10-03 22:31:29 +0800

    GeloSharvien

--- Website/Gallery.php
+++ Website/Gallery.php
@@ -42,13 +42,16 @@
         Openning Time
 </div>
 <div class="right-subtext">Tuesday - Sunday (8:00AM - 4:00PM)</div>
-<div class="right-subtext2">See full</div>
+<div class="right-subtext2">See full <b>Opening hours</b></div>
         <div><button class="right-btn">View Museum Map <svg class="right-arrow" style="width:24px;height:24px" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M6,13V11H14L10.5,7.5L11.92,6.08L17.84,12L11.92,17.92L10.5,16.5L14,13H6M22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2A10,10 0 0,1 22,12M20,12A8,8 0 0,0 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12Z" />
                 </svg></button></div>
 </div>
 </div>
 </div>
+
+
+
 </main>
 
 <footer class="footer-content">
