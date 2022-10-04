04cc0c242f84631515379cf60badef9f5df2eafa
jheltapang <113102939+jheltapang@users.noreply.github.com>
2022-10-04 21:50:33 +0800

    Gallery Info

--- Website/GalleryInfo.php
+++ Website/GalleryInfo.php
@@ -2,7 +2,7 @@
 <html>
 <head>
         <title>Gallery Info Page</title>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="assets/css/galleryinfo.css">
 <link href="https://fonts.googleapis.com/css2?family=Lora:ital@1&display=swap" rel="stylesheet">
 <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 </head>
@@ -18,31 +18,119 @@
                         <li><a href="#" class="search-icon">Search</a></li>
                 </ul>
                 </nav>
-        <div class="GalleryInfo-text">Gallery Name</div>         
+            
 </header>
+<div class="GalleryInfo-text">Gallery Name</div>     
 <main class="main-content-plan">
 <div class="plan-flex">
 <div class="left-content">
-<div class="gen-text">qwe qt qrsad asqr qw asfasdaw dasdqwe  qweasd asqw qewasd fq qwed adqwe rfasqwe qwr asdasdqt hdfgdfg er w <br> Museum from home.</div>
+<div class="gen-text">qwe qt qrsad asqr qw asfasdaw dasdqwe  qweasd asqw qewasd fq qwed adqwe rfasqwe qwr asdasdqt hdfgdfg qwe dsadqwe kqwjek sdapqwe okasdj qwedan qwje qkew asdasd qwqkewle kql ;kqw;le kq;lwe oqwe ids daskjdh jas qweh jqkwhe er wasdasd irqiwe nd asjdqw hqwueh saoqwejqksd qhweq jheksadhqwe h sdasdasjdaks hasdk haskjqwga sjdkj asdjasdhjash qjwh jahsdja qwjeh jqwh ajsd jqhw ejas  hakjsd hajs dh qlwe jkasdjqagdshjgqw jasdhg jasdqwikhd kashdkqw hkagsdkjqgwd kjahsdkqwhdoe l halsdhklqawhkjas  hqwjhe kjas dh </div>
 
 
 </div>
 
-
-
-
 </div>
 </div>
 </div>
-<!---------Card1--------->
+<!--Gallery Name-->
+<div class="main">
+<div class="cards">
+        <div class="image">
+        <img src="Images/gallery/Artifact1.jpg">
+</div>
+<div class="title">
+        <h1>Display Name</h1>
+</div>
+<div class="des">
 
-<!---------Card2--------->
+<p>Year</p>
+<br>
+<p>Artifact</p>
+</div>
+</div>
 
+<div class="cards">
+        <div class="image">
+        <img src="Images/gallery/Artifact2.jpg">
+</div>
+<div class="title">
+        <h1>Display Name</h1>
+</div>
+<div class="des">
 
-<!---------Card3--------->
+<p>Year</p>
+<br>
+<p>Artifact</p>
+</div>
+</div>
 
+<div class="cards">
+        <div class="image">
+        <img src="Images/gallery/Artifact3.jpg">
+</div>
+<div class="title">
+        <h1>Display Name</h1>
+</div>
+<div class="des">
+
+<p>Year</p>
+<br>
+<p>Artifact</p>
+</div>
+</div>
+</div>
+
+<div class="cards">
+        <div class="image">
+        <img src="Images/gallery/Artifact4.jpg">
+</div>
+<div class="title">
+        <h1>Display Name</h1>
+</div>
+<div class="des">
+
+<p>Year</p>
+<br>
+<p>Artifact</p>
+</div>
+</div>
+</div>
+
+<div class="cards">
+        <div class="image">
+        <img src="Images/gallery/Artifact5.jpg">
+</div>
+<div class="title">
+        <h1>Display Name</h1>
+</div>
+<div class="des">
+
+<p>Year</p>
+<br>
+<p>Artifact</p>
+</div>
+</div>
+</div>
+
+<div class="cards">
+        <div class="image">
+        <img src="Images/gallery/Artifact6.jpg">
+</div>
+<div class="title">
+        <h1>Display Name</h1>
+</div>
+<div class="des">
+
+<p>Year</p>
+<br>
+<p>Artifact</p>
+</div>
+</div>
+</div>
 
 </main>
+
+
 
 <footer class="footer-content">
  <table class="left-footer box">
@@ -74,7 +162,6 @@
                         <svg style="width:24px;height:24px" viewBox="0 0 24 24">
                                    <path fill="currentColor" d="M10,15L15.19,12L10,9V15M21.56,7.17C21.69,7.64 21.78,8.27 21.84,9.07C21.91,9.87 21.94,10.56 21.94,11.16L22,12C22,14.19 21.84,15.8 21.56,16.83C21.31,17.73 20.73,18.31 19.83,18.56C19.36,18.69 18.5,18.78 17.18,18.84C15.88,18.91 14.69,18.94 13.59,18.94L12,19C7.81,19 5.2,18.84 4.17,18.56C3.27,18.31 2.69,17.73 2.44,16.83C2.31,16.36 2.22,15.73 2.16,14.93C2.09,14.13 2.06,13.44 2.06,12.84L2,12C2,9.81 2.16,8.2 2.44,7.17C2.69,6.27 3.27,5.69 4.17,5.44C4.64,5.31 5.5,5.22 6.82,5.16C8.12,5.09 9.31,5.06 10.41,5.06L12,5C16.19,5 18.8,5.16 19.83,5.44C20.73,5.69 21.31,6.27 21.56,7.17Z" />
                                </svg></div>
-<img class="icon-casareal" src="Images/IconCasaReal.png">
 </div>
 <div class="right-footer box">
 <h2 class="Contact-info">Contact Information</h2>
