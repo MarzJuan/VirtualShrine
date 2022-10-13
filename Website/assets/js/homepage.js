b0d74de15bce738301a99d9784a5ab98b6310ba3
RoviK31 <rovikeith.navarro.y@bulsu.edu.ph>
2022-10-13 14:04:04 +0800

    carousel 2.0
    Note: dipa gumagana indicator

--- Website/assets/js/homepage.js
+++ Website/assets/js/homepage.js
@@ -14,13 +14,15 @@ navToggle.addEventListener("click", () => {
 });
 
 
+const dotsNav = document.querySelector(".carousel-nav");
+const dots = Array.from(dotsNav.children);
+/* 
 //carousel
 const track = document.querySelector(".carousel-track");
 const slides = Array.from(track.children);
 const nextButton = document.querySelector(".carousel-button-right");
 const prevButton = document.querySelector(".carousel-button-left");
-const dotsNav = document.querySelector(".carousel-nav");
-const dots = Array.from(dotsNav.children);
+
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 
@@ -72,4 +74,22 @@ dotsNav.addEventListener ('click', e => {
   const targetSlide =  slide[targetIndex];
   
   moveToSlide(track, currentSlide, targetSlide);
-});
+});*/
+//carousel 2.0
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach(button => {
+        button.addEventListener("click", () =>{
+            const offset = button.dataset.carouselButton === "next" ? 1 : -1
+            const slides = button.closest("[data-carousel]").querySelector('[data-slides]')
+
+            const activeSlides =  slides.querySelector('[data-active]')
+            let newIndex = [...slides.children].indexOf(activeSlides) + offset
+            if(newIndex < 0 ) newIndex = slides.children.length - 1 
+            if(newIndex >= slides.children.length) newIndex= 0 
+
+            slides.children[newIndex].dataset.active= true
+            delete activeSlides.dataset.active
+    })
+})
