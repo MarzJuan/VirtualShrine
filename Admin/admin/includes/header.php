44b3300b8ee141e765f5899c91e00d7ba9325f53
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-16 21:34:16 +0800

    Update header.php

--- Admin/admin/includes/header.php
+++ Admin/admin/includes/header.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>VirtualShrine - Admin</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
