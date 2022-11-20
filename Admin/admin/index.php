961b0e7eb08bab87677b47de4e6fe70db7cda60a
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-20 10:51:15 +0800

    .

--- Admin/admin/index.php
+++ Admin/admin/index.php
@@ -22,7 +22,7 @@ include('includes/header.php');
     <div class="col-lg-8">
       <div class="row">
 
-        <!-- Sales Card -->
+        <!-- BOOKINGS CARD -->
         <a href="booking-approved.php">
         <div class="col-xxl-4 col-md-6">
           <div class="card info-card sales-card">
@@ -56,25 +56,12 @@ include('includes/header.php');
             <!-- <a href="booking-approved.php"><span style="margin-left:5rem;">view more</span></a> -->
           </div>
           </a>
-        </div><!-- End Sales Card -->
+        </div><!-- End BOOKINGS CARD -->
         
 
-        <!-- Revenue Card -->
+        <!-- WEBSITE VISITS CARD -->
         <div class="col-xxl-4 col-md-6">
           <div class="card info-card revenue-card">
-
-            <!-- <div class="filter">
-              <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
-
-                <li><a class="dropdown-item" href="#">Today</a></li>
-                <li><a class="dropdown-item" href="#">This Month</a></li>
-                <li><a class="dropdown-item" href="#">This Year</a></li>
-              </ul>
-            </div> -->
 
             <div class="card-body">
               <h5 class="card-title">Website Visits</h5>
@@ -84,32 +71,18 @@ include('includes/header.php');
                   <i class="bi bi-binoculars"></i>
                 </div>
                 <div class="ps-3">
-                  <h6>$3,264</h6>
-                  <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>
-
+                  <h6  id="result-visits"></h6>
                 </div>
               </div>
             </div>
           </div>
-        </div><!-- End Revenue Card -->
+        </div>
+        <!-- WEBSITE VISITS CARD -->
 
         <!-- Customers Card -->
-        <div class="col-xxl-4 col-xl-12">
+        <div class="col-xxl-4 col-xl-12" id="active-visitor">
 
           <div class="card info-card customers-card">
-
-            <!-- <div class="filter">
-              <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-              <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <li class="dropdown-header text-start">
-                  <h6>Filter</h6>
-                </li>
-
-                <li><a class="dropdown-item" href="#">Today</a></li>
-                <li><a class="dropdown-item" href="#">This Month</a></li>
-                <li><a class="dropdown-item" href="#">This Year</a></li>
-              </ul>
-            </div> -->
 
             <div class="card-body">
               <h5 class="card-title">Visitors</h5>
@@ -617,6 +590,59 @@ include('includes/header.php');
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
+
+<!-- SCRIPT FOR GOOGLE ANALYTICS -->
+
+<script type="text/javascript">
+  setInterval(function(){
+    call();
+  },5000);
+  function call(){
+    get('visits');
+    get('visitor');
+    get('devices');
+    get('sources');
+    get('countries');
+    get('browser');
+    get('os');
+  }
+  call();
+  function get(action){
+    var view = '<?php echo VIEW;?>';
+    $.ajax({
+      url:"ajax.php?action="+action+'&view='+view,
+      type:'get',
+      success:function(res){
+        if(action=='pages'){
+          $("#result-visits tbody").html(res);
+        }
+        else if(action=='visitors'){
+          $("#active-visitor").html(res);
+        }
+        else if(action=='devices'){
+          $("#devices").html(res);
+        }
+        else if(action=='sources'){
+          $("#result-sources tbody").html(res);
+        }
+        else if(action=='countries'){
+          $("#countries-sources tbody").html(res);
+        }
+        else if(action=='browser'){
+          $("#browser-sources tbody").html(res);
+        }
+        else if(action=='os'){
+          $("#os-sources tbody").html(res);
+        }
+      }
+    });
+  }
+  $(document).on('click','.open-link',function(){
+    link = $(this).attr('data-link');
+    link = '<?php echo DOMAIN;?>'+link;
+    window.open(link, '_blank');
+  });
+</script>
 
 <?php
 include('includes/footer.php');
