818aee243599cf19249154b29ea2697afe1bb26c
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-20 22:27:17 +0800

    Update index.php

--- Admin/admin/index.php
+++ Admin/admin/index.php
@@ -104,6 +104,140 @@ include('includes/header.php');
 
         </div><!-- End Customers Card -->
 
+        <!-- ------------------------------------------------------------------------------------------- -->
+
+        <div class="container page-content">
+  <div class="row">
+    <div class="col-md-8">
+      <table class="table table-bordered" id="result-pages">
+        <thead>
+          <tr>
+            <th width="40%">Top Active Pages</th>
+            <th>Users</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    </div>
+    <div class="col-md-4">
+      <table class="table table-bordered">
+              <tbody>
+                <tr>
+                    <th>Right Now</th>
+              </tr>
+                <tr>
+                    <td>
+                      <h1>
+                                <div class="count" id="active-users">
+                                </div>
+                      </h1>
+                      <p>Active Users on Site</p>
+                      <br>
+                      <div id="devices">
+                     </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          
+          <table class="table table-bordered" id="result-sources">
+            <thead>
+              <tr>
+                <th>Source</th>
+                <th>Users</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+          
+          <table class="table table-bordered" id="countries-sources">
+            <thead>
+              <tr>
+                <th>Country</th>
+                <th>Users</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+          <table class="table table-bordered" id="browser-sources">
+            <thead>
+              <tr>
+                <th>Browser</th>
+                <th>Users</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+          <table class="table table-bordered" id="os-sources">
+            <thead>
+              <tr>
+                <th>OS</th>
+                <th>Users</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+          
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+  setInterval(function(){
+    call();
+  },5000);
+  function call(){
+    get('pages');
+    get('users');
+    get('devices');
+    get('sources');
+    get('countries');
+    get('browser');
+    get('os');
+  }
+  call();
+  function get(action){
+    var view = '<?php echo VIEW;?>';
+    $.ajax({
+      url:"ajax.php?action="+action+'&view='+view,
+      type:'get',
+      success:function(res){
+        if(action=='pages'){
+          $("#result-pages tbody").html(res);
+        }
+        else if(action=='users'){
+          $("#active-users").html(res);
+        }
+        else if(action=='devices'){
+          $("#devices").html(res);
+        }
+        else if(action=='sources'){
+          $("#result-sources tbody").html(res);
+        }
+        else if(action=='countries'){
+          $("#countries-sources tbody").html(res);
+        }
+        else if(action=='browser'){
+          $("#browser-sources tbody").html(res);
+        }
+        else if(action=='os'){
+          $("#os-sources tbody").html(res);
+        }
+      }
+    });
+  }
+  $(document).on('click','.open-link',function(){
+    link = $(this).attr('data-link');
+    link = '<?php echo DOMAIN;?>'+link;
+    window.open(link, '_blank');
+  });
+</script>
+
+        <!-- ------------------------------------------------------------------------------------------- -->
         <!-- Reports -->
         <div class="col-12">
           <div class="card">
