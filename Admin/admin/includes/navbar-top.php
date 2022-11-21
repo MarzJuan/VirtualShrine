2b25c1603df75777d3bcff42caa961cf8a5f6095
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-21 14:36:11 +0800

    Update navbar-top.php

--- Admin/admin/includes/navbar-top.php
+++ Admin/admin/includes/navbar-top.php
@@ -96,30 +96,13 @@
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
           
           <!-- TOP NAV USER ICON PICTURE -->
-          <?php
-          if(isset($_GET['id']))
-              {  
-                  $user_id = $_GET['id'];
-                  $users = "SELECT * FROM users WHERE id='$user_id'";
-                  $user_run = mysqli_query($con, $users);
 
-
-                  if(mysqli_num_rows($user_run) > 0)
-                  {
-                      while($user = mysqli_fetch_assoc($user_run))
-                      {
-                      ?>
           <?php
-            $image = $user['profileImage'];
+            $image = $_SESSION['auth_user']['profileImage'];
             if (empty($image))
             $image = "../../uploads/user/Default_pfp.jpeg";
             
             echo '<img src="../uploads/user/'.$image.'" alt="Profile" class="rounded-circle">'
-          ?>
-          <?php
-                      }
-                    }
-                }
           ?>
 
            <!-- TOP NAV USER ICON PICTURE -->
