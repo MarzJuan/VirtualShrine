49f11b2e53250f30134143c833f96a60902fea3a
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-21 21:50:09 +0800

    Update booking-pending.php

--- Admin/admin/booking-pending.php
+++ Admin/admin/booking-pending.php
@@ -156,11 +156,6 @@ include('includes/header.php');
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../../Website/assets/js/main.js?v1"></script>
-    <script src="../../Website/assets/js/parsely.min.js"></script>
-    <script src="../../Website/assets/js/popper.min.js"></script>
 
 
 <?php
