11cfc86cebc17ad63c67703e13f864c7161e5740
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-23 00:03:12 +0800

    Update ReschedCancel_details.php

--- Website/ReschedCancel_details.php
+++ Website/ReschedCancel_details.php
@@ -63,7 +63,7 @@ include('config/dbcon.php');
                         <p style="margin-left:115px;">Enter Booking ID:</p>
                             <div style="margin-left:100px;" class="row">
                                 <div class="col-md-8">
-                                    <input type="text" name="booking_id" value="<?php if(isset($_GET['bookingID'])){echo $_GET['bookingID'];} ?>" class="form-control">
+                                    <input type="text" name="book" value="<?php if(isset($_GET['book'])){echo $_GET['bookingID'];} ?>" class="form-control">
                                 </div>
                                 <div class="col-md-4">
                                     <button type="submit" class="btn btn-primary">Search</button>
@@ -76,7 +76,7 @@ include('config/dbcon.php');
                                 <hr>
                                 <?php
 
-                                    if(isset($_GET['bookingID']))
+                                    if(isset($_GET['book']))
                                     {
                                         $booking_id = $_GET['bookingID'];
 
