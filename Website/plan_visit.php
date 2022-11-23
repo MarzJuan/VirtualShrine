0b07378ad009727ee2cb7fd27d3c8360a528be0d
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-23 21:37:43 +0800

    Update plan_visit.php

--- Website/plan_visit.php
+++ Website/plan_visit.php
@@ -79,7 +79,7 @@
             <p></p>
             <h5>Museum Operation Details</h5><br>
             <p>The museum is open every <b>Tuesday - Sunday</b> from <b>8:00 am to 4:00 pm</b>. Make sure to follow the rules and 
-            regulation being implemented inside the museum.</p><br>
+            regulation being implemented inside the museum.</p>
             <hr>
             <br>
             <h5>Booking Online</h5><br>
