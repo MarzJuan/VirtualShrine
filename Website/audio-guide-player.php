192348cee776b2b5521d6dbdc1b37e4c07709d91
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-24 05:02:08 +0800

    AUDIO PLAYER

--- Website/audio-guide-player.php
+++ Website/audio-guide-player.php
@@ -34,40 +34,7 @@
             </h1>
         </div>
 
-        <section class="design">
-            
-<img src="https://i.pinimg.com/originals/83/82/06/83820619a50bc4676d642561a9fe7355.gif" alt="Smiley face" width=400 >
-</section> 
-<section class="implementation">
-  <div class="now-playing-board" id="now-playing-board-id">
-    <div class="now-playing-details">
-      <h4>Radio GaGa</h4>
-      <h6 style="color:rgb(71,71,71);">Queen</h6>
-      <div class="slidecontainer-one">
-        <input type="range" min="1" max="100" value="1" class="slider" id="range">
-        <div id="completionBar"></div>
-      </div>
-    </div>
-  </div>
-  <div class="now-playing-board-bottom-bar" id="now-playing-board-bottom-bar-id">
-  </div>
-  <div class="play-controls">
-    <img src="https://pngimg.com/uploads/vinyl/vinyl_PNG21.png" width=100 height=100 id="vynl-id" class="vynl"/>
-    <div class="play-buttons">
-            <i class='fas fa-step-backward' style='font-size:20px'></i>
-            <div class='play-circle' id="playpause" onclick="play(this)"></div>
-            <i class='fas fa-step-forward' style='font-size:20px'></i>
-    </div>	
-    
-    <div class="volume-button">
-      <i class='fas fa-volume-up' style='font-size:15px'></i>
-    </div>
-  </div>
-  <audio id="audioSrc">
-  <source src="http://rajanlabs.000webhostapp.com/assets/Radio%20Ga%20Ga.mp3" type="audio/mp3">
-</audio>
-</section>
-
+        
     </div>   
 </div>
 
