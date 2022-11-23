3357266755bf9021a571c924c0405250342ac641
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-24 04:08:59 +0800

    Audio player

---
+++ Website/audio-guide-player.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <head>
+        <title>VirtualShrine - Audio Guide</title>
+        <link rel="stylesheet" href="assets/css/audio-guide-player.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="icon" type="image/png" href="assets/img/crs-logo.png">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" charset="utf-8"></script>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+<!-- push -->
+    <section class="header">
+        <nav>
+            <a href="homePage.php"><image  class="logo" src="Images/Logo.png" alt="Casa Real Shrine logo"></image></a>
+        </nav>
+    </section>
