64c7f3ae224507067cf45fa9643ae7f25d8d09d5
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-23 11:30:23 +0800

    Update sidebar.php

--- Admin/admin/includes/sidebar.php
+++ Admin/admin/includes/sidebar.php
@@ -27,7 +27,7 @@
 
   <li class="nav-item">
   <?php
-              $sql = "SELECT * FROM  bookings WHERE status='0' ORDER BY booking_id DESC";
+              $sql = "SELECT * FROM  bookings WHERE status='0' AND status='3'";
               $res = mysqli_query($con, $sql);
           ?>
     <a class="nav-link collapsed <?= $page == 'booking-pending.php' || $page == 'booking-approved.php' || $page == 'booking-rejected.php' || $page == 'booking-cancelled.php' || $page == 'booking-details.php' || $page == 'booking-details-reject.php' || $page == 'booking-details-cancelled.php' || $page == 'booking-details-approved.php' ? 'active':'' ?>" 
