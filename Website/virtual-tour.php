4baeca61850abbc6ee64dcbb63b11f8e9273469a
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-27 11:09:52 +0800

    VIRTUAL TOUR

--- Website/virtual-tour.php
+++ Website/virtual-tour.php
@@ -20,10 +20,7 @@
             <source src="video/casareal.mp4" type="video/mp4">
         </video>
 
-        <div class="virtual360-btn">
-        <a href="../VirtualTours/index.html"><button type="button">Go to Virtual 360</button></a>
-       </div>
-            <a href="homePage.php"><image  class="logo" src="Images/Logo.png" alt="Casa Real Shrine logo"></image></a>
+       <a href="homepage.php"><image  class="logo" src="Images/Logo.png" alt="Casa Real Shrine logo"></image></a>
             <div class="nav-links" id="mySidebar">
             <i class="fa fa-times" onclick="closeNav()"></i>
                 <ul>
@@ -61,12 +58,119 @@
 
         <!-- [START] HEADER -->
         <div class="text-box">
-            <h1>WELCOME TO THE VIRTUALSHRINE</h1>
-            <p>Free Entry - Operation time : 8:00AM - 4:00PM</p>
-            <a class="book-btn" href="consent-form.php">Book a Visit Now</a>
+            <h1 class="expressive">Explore the Casa Real Shrine</h1>
+            <!-- <h3 class="jumplink-banner-description"><p>Explore the Political history of the Philippines with VirtualShrine.</p> -->
+            <a class="virtual-btn" href="https://virtualshrine.online/VirtualTours/index.html">Go to Virtual 360</a>
         </div>
         <!-- [END] OF HEADER -->
     </section>
+
+    <!-- VIRTUAL TOUR TIPS -->
+
+    <section class="content-split content-admission">
+    <div class="content-wrap">
+        <div class="content-split-main rich-text">
+            <p></p>
+            <h4>Enjoy the Virtual360 Tour</u></h4><br>
+            <div class="split-main-indent" style="">
+            The VirtualShrine provides a virtual tour that allow visitors to enjoy self-guided, 
+            room-by-room tours of certain exhibits and gallery inside the museum from their PC or mobile 
+            device.
+            </div>
+            <br>
+            <div class="split-main-card" style="">
+                <p><b>Note: </b>This tour and these presentations have been thoroughly tested and should 
+                    function on all commonly used devices and browsers (using a desktop computer with Windows, 
+                    Mac, Linux or a mobile device such as an iPhone, iPad, or Android). Its functionality and 
+                    design may change as it adjusts automatically to accommodate the greatest number of visitors.
+                </p>
+                </div>
+            <br>
+        </div>
+        <div class="rich-text content-split-side">
+            <ul id="accordion">
+                <li>
+                    <label for="first">Virtual tour tips <span>&#x3e;</span></label>
+                    <input type="radio" name="accordion" id="first">
+                    <div class="accordion_content">
+                        <ul>
+                            <li style="list-style: disc outside none;display: list-item;margin-left: 1em;">
+                                To navigate between galleries in the tour, click the thumbnail menu to display all galleries.
+                            </li>
+                            <li style="list-style: disc outside none;display: list-item;margin-left: 1em;">
+                                Click the gallery you want to explore, each gallery is shown in 360 view.
+                            </li>
+                            <li style="list-style: disc outside none;display: list-item;margin-left: 1em;">
+                                Click the arrow provided in order to go to the next gallery.
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <li>
+                    <label for="second">Virtual 360 Functionalities <span>&#x3e;</span></label>
+                    <input type="radio" name="accordion" id="second">
+                    <div class="accordion_content">
+                        <ul>
+                            <li style="list-style: disc outside none;display: list-item;margin-left: 1em;">
+                                <b>Hotspots </b>- Click the hotspot to display further information regarding the display.
+                                The hotspots can bee seen on every display.
+                            </li>
+                            <li style="list-style: disc outside none;display: list-item;margin-left: 1em;">
+                                <b>Arrows </b>- This virtual tour is designed for gradual movement that mimics walking. Clicking the arrow 
+                                will let you jump around different locations at a moment's notice.
+                            </li>
+                            <li style="list-style: disc outside none;display: list-item;margin-left: 1em;">
+                                <b>Rotate and Zoom </b>- You can explore the 360-degree view by swiping the screen from left to right.
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+            <a href="plan_visit.php"><button class="button-31" role="button">Plan Your Visit</button></a>
+            <a href="consent-form.php"><button class="button-31" role="button">Schedule a Visit</button></a>
+
+            </div>
+
+        </div>
+    </div>
+    </section>
+
+    <hr>
+    <!-- VIRTUAL TOUR TIPS -->
+
+    <!-- OTHER CONTENT -->
+
+    <section class="gallery-rows">
+    <h4>You may also be interested in</u></h4><br>
+    <div class="row">
+        <a style="text-decoration: none;color: black;" href="">
+            <div class="features-col">
+                <div id="container">
+                <img id="image" src="assets/css/image/gallery-collection.jpg" alt="">
+                </div>
+                <div class="featured-text-title">
+                    <h3 class="featured-title">Collections</h3>
+                    <p>Explore the 4 Galleries of Casa Real Shrine</p>
+                </div>
+            </div>
+        </a>
+
+        <a style="text-decoration: none;color: black;" href="">
+            <div class="features-col">
+                <div id="container">
+                <img id="image" src="Images/VirtualTour/philippine_president.jpg" alt="">
+                </div>
+                <div class="featured-text-title">
+                    <h3 class="featured-title">Philippine Presidents</h3>
+                    <p>This Collection displays all the Presidents in the Philippines.</p>
+                </div>
+            </div>
+        </a>
+    </div>
+    </section>
+
+    <!-- OTHER CONTENT -->
 
 
 
@@ -106,6 +210,22 @@
 
     <!-- [END] FOOTER -->
 
+    <!-- JAVASCRIPT FOR ACCORDION -->
+<script>
+    var allRadios = document.getElementsByName('accordion');
+    var booRadio;
+    var x = 0;
+    for(x = 0; x < allRadios.length; x++){
+    allRadios[x].onclick = function() {
+        if(booRadio == this){
+        this.checked = false;
+        booRadio = null;
+        } else {
+        booRadio = this;
+        }
+    };
+    }
+</script>
 
 
     <!-- JAVASCRIPT FOR TOGGLE MENU -->
