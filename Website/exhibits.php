51ff1c3bcd5b4d575af44508e368953974c7b953
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-11-29 01:26:37 +0800

    exhibits and history

--- Website/exhibits.php
+++ Website/exhibits.php
@@ -2,8 +2,8 @@
 <html>
     <meta name="viewport" content="with=device-width, initial-scale=1.0">
     <head>
-        <title>History - VirtualShrine</title>
-        <link rel="stylesheet" href="assets/css/casa-real-history.css">
+        <title>Exhibits - VirtualShrine</title>
+        <link rel="stylesheet" href="assets/css/exhibits.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="icon" type="image/png" href="assets/img/crs-logo.png">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -53,35 +53,118 @@
         <!-- [END] NAVIGATION -->
     </section>
 
-    <!-- [START] HISTORY OF CASA REAL SHRINE -->
-    <section>
-    <div class="main-wraper wrapper">
-        <div class="row">
-            <div class="med-hide lrg-c2">
-                <div class="content content-center">
-                    <h1 class="page-title">History of the Museum</h1>
-                    <div class="rich-text z-container">
-                        <p></p>
-                        <h2>Casa Real Shrine of Malolos</h2>
 
-                        <div class="image-about">
-                        <img src="Images/about/old-crs.jpg" alt="Casa Real Shrine">
-                        </div>
-                        <p>The Casa Real Shrine was built in 1580 and was stablished in 1767 – 1768 as the First Government Structure at Malolos.  Casa Real served as the House of Tribunal, Municipal House of Tribunal, Hall of Justice and Municipal Hall at different occasions. This also served as the the residence and office of the Gobernadorcillo during the Spanish time and office of those who were elected as Governors of Malolos from 1868 to 1892.</p>
-                        <p>The building had witnessed the war between Filipinos and the Spaniards in 1896, and in 1898-1899 the patriotic struggle of Filipinos that were documented through La Independencia and Heraldo de La Revolucion that were printed by the National Printing Press that was maintained inside the building through a decree that was issued by the Congress of Malolos. A part of this building was also used as office of the National Treasury (1898-1899).</p>
-                        <p>In 1965, the building was demolished to give way to a commercial project that was built in the same place.  However, the significance of this building as part of the golden history of Malolos was maintained when President Diosdado Macapagal declared the Casa Real a historic shrine, on October 4, 1965 through Presidential Decree No. 173.</p>
-                        <p>This printing press was used from 1898 to 1899 under the press director, Zacarias Fajardo. After the end of the war against the United States, Bulacan revolutionary, Mariano Crisostomo took care of the printing press in his Imprenta Plaridel in Malolos</p>
-                        <p>And in 1997 the government purchased the printing press and brought it back on its former home to remind the youth of the humble beginnings of our nation. This press stands as one of the symbols of our nation’s quest of freedom---the core of our pollical history.</p>
-                        <p>At present, the building which was restored to its original form and is under the management of the National Historical Institute and serves as the final repository of still existing memorabilia. This structure played a gigantic role in Philippine Republic.  One of the permanent exhibits of Casa Real Shrine are relics of the Brave Women of Malolos of whom Rizal wrote a poem to honor their courage of setting up a school against objections from the towns friars.</p>
-                        <p class="inline-caption"></p>
-                    </div>
-                </div>
-            <div class="med-hide lrg-c2">
-            </div>
-        </div>
+<!-- [START] EXHIBIT CONTENTS -->
+
+<section class="exhibit-body">
+
+<h1 class="exhibit-title">
+    Exhibitions
+</h1>
+<div class="tabset">
+  <!-- Tab 1 -->
+  <input type="radio" name="tabset" id="tab1" aria-controls="marzen" checked>
+  <label for="tab1">Current</label>
+  <!-- Tab 2 -->
+  <input type="radio" name="tabset" id="tab2" aria-controls="rauchbier">
+  <label for="tab2">Upcoming</label>
+  <!-- Tab 3 -->
+  <input type="radio" name="tabset" id="tab3" aria-controls="dunkles">
+  <label for="tab3">Past</label>
+  
+  <!-- CURRENT EXHIBITS -->
+  <div class="tab-panels">
+    <section id="marzen" class="tab-panel">
+    <!-- cards -->
+    <div class="blog-card">
+    <div class="meta">
+      <div class="photo" style="background-image: url(https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg)"></div>
+      <ul class="details">
+        <li class="author"><a href="#">Room</a></li>
+        <li class="date">Date of Exhbit</li>
+        <li class="tags">
+          <ul>
+            <li><a href="#">Exhibit Name</a></li>
+          </ul>
+        </li>
+      </ul>
     </div>
+    <div class="description">
+      <h1>Exhibit Name</h1>
+      <h2>Slogan for Exhibit</h2>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
+      <p class="read-more">
+        <a href="#">Read More</a>
+      </p>
+    </div>
+  </div>
+  <!-- cards -->
     </section>
-    <!-- [END] HISTORY OF CASA REAL SHRINE -->
+
+    <!-- UPCOMING EXHIBITS -->
+    <section id="rauchbier" class="tab-panel">
+    <!-- cards -->
+    <div class="blog-card">
+    <div class="meta">
+      <div class="photo" style="background-image: url(https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg)"></div>
+      <ul class="details">
+        <li class="author"><a href="#">Room</a></li>
+        <li class="date">Date of Exhbit</li>
+        <li class="tags">
+          <ul>
+            <li><a href="#">UPCOMING Exhibit Name</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div class="description">
+      <h1>UPCOMING Exhibit Name</h1>
+      <h2>Slogan for Exhibit</h2>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
+      <p class="read-more">
+        <a href="#">Read More</a>
+      </p>
+    </div>
+  </div>
+    <!-- cards -->
+    </section>
+
+    <!-- PAST EXHIBITS -->
+    <section id="dunkles" class="tab-panel">
+      <!-- cards -->
+    <div class="blog-card">
+    <div class="meta">
+      <div class="photo" style="background-image: url(https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg)"></div>
+      <ul class="details">
+        <li class="author"><a href="#">Room</a></li>
+        <li class="date">Date of Exhbit</li>
+        <li class="tags">
+          <ul>
+            <li><a href="#">PAST Exhibit Name</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div class="description">
+      <h1>PAST Exhibit Name</h1>
+      <h2>Slogan for Exhibit</h2>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
+      <p class="read-more">
+        <a href="#">Read More</a>
+      </p>
+    </div>
+  </div>
+    <!-- cards -->
+    </section>
+
+  </div>
+  
+</div>
+
+</section>
+<!-- [END] EXHIBIT CONTENTS -->
+
+
 
 
 
