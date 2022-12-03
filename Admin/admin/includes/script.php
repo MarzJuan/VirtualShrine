efa167c8690a03df69ae7b5325a1a0dd0e45c629
kristhayne <kristinejoy.nacional.s@bulsu.edu.ph>
2022-12-03 10:08:43 +0800

    language

--- Admin/admin/includes/script.php
+++ Admin/admin/includes/script.php
@@ -72,5 +72,30 @@
     </script>
   
 
+  <!-- FORM TABS -->
+
+  <script>
+    function openLanguage(evt, languageType) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(languageType).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+  </script>
+
 </body>
 </html>
