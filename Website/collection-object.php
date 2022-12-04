f33d5a0c3948210cff28220d90be8ccdb91d6ec7
kristhayne <kristinejoy.nacional.s@bulsu.edu.ph>
2022-12-04 17:45:42 +0800

    RESIZE LOGO

--- Website/collection-object.php
+++ Website/collection-object.php
@@ -23,7 +23,7 @@ include('config/dbcon.php');
 
 <section class="header">
         <nav>
-            <a href="homepage.php"><image  class="logo" src="Images/Logo.png" alt="Casa Real Shrine logo"></image></a>
+            <a href="homepage.php"><image  class="logo" src="assets/img/crs-logo.png" alt="Casa Real Shrine logo"></image></a>
             <div class="nav-links" id="mySidebar">
             <i class="fa fa-times" onclick="closeNav()"></i>
                 <ul>
