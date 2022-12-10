b9ad0de372e003ddcc2eb827a5bacbea20423067
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-12-10 13:25:02 +0800

    Update exhibit-display-edit.php

--- Admin/admin/exhibit-display-edit.php
+++ Admin/admin/exhibit-display-edit.php
@@ -83,7 +83,7 @@ include('includes/header.php');
 
                             <div class="col-md-12 mb-3">
                                 <label for="">Description</label>
-                                <textarea name="description" id="summernote" required class="form-control" rows="4"><?= $post_row['description'] ?></textarea>
+                                <textarea name="description" required class="form-control summernote" rows="4"><?= $post_row['description'] ?></textarea>
                             </div>
 
                             <div class="col-md-6 mb-3">
