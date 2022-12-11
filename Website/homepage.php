fca19878c64bbee771016b7078b9f88bd4e33be5
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-12-11 11:53:39 +0800

    Update homepage.php

--- Website/homepage.php
+++ Website/homepage.php
@@ -165,6 +165,7 @@
         <p>Explore our virtual tours and experience the Casa Real wherever you are</p>
 
         <div class="row">
+            <a href="">
             <div class="features-col">
             <div id="container">
                 <img id="image" src="Images/homepage-features/image1.jpg" alt="">
@@ -174,6 +175,7 @@
                 <p>Explore Casa Real with an Audio Guide</p>
             </div>
             </div>
+            </a>
 
             <div class="features-col">
             <div id="container">
