3cc5971567eff18662599af5707d13bb48923fb7
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-12-14 01:05:35 +0800

    Update code-superAdmin.php

--- Admin/admin/code-superAdmin.php
+++ Admin/admin/code-superAdmin.php
@@ -124,7 +124,7 @@ if(isset($_POST['display_archive']))
 {
     $display_id = $_POST['display_archive'];
     // 2 = Archived
-    $query = "UPDATE exhibit_display SET status='2' WHERE display_id='$display_id' LIMIT 1";
+    $query = "UPDATE blog SET status='2' WHERE display_id='$display_id' LIMIT 1";
     $query_run = mysqli_query($con, $query);
 
     if($query_run)
