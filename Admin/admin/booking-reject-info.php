a93e214cc76a809a892af8cdbd77e13acbb94cf1
Kristine Joy Nacional <113058852+kristhaynee@users.noreply.github.com>
2022-12-15 14:19:04 +0800

    Update booking-reject-info.php

--- Admin/admin/booking-reject-info.php
+++ Admin/admin/booking-reject-info.php
@@ -17,10 +17,28 @@ include('includes/header.php');
                 </div>
                 <div class="card-body">
 
+                <?php
+                    if(isset($_GET['booking_id']))
+                    {
+                        $booking_id = $_GET['booking_id'];
+                        $booking_query = "SELECT * FROM bookings WHERE booking_id='$booking_id' LIMIT 1";
+                        $booking_query_res = mysqli_query($con, $booking_query);
+
+                        if(mysqli_num_rows($booking_query_res) > 0)
+                        {
+                            $booking_row = mysqli_fetch_array($booking_query_res);
+                            ?>
+
 
                 <form action="code.php" method="POST" enctype='multipart/form-data'>
 
                     <div class="row">
+
+                            <div style="margin-left:50px;" class="form-group mb-3">
+                                <label for=""><b>Booking ID:</b></label><a style="margin-left:57px;">
+                                    <?php echo $booking_row['bookingID']; ?></a>
+                            </div>
+
                             <div class="col-md-12 mb-3">
                                 <label for="">Why do you want to reject this booking?</label>
                                 <textarea type="text" name="reason" required class="form-control"></textarea>
@@ -30,6 +48,10 @@ include('includes/header.php');
                             <div class="com-md-6 mb-3">
                                 <button type="submit" name="add_category" class="btn btn-primary">Save Gallery</button>
                             </div>
+                            <?php
+                        }
+                    }
+                    ?>
 
 
                         </div>
